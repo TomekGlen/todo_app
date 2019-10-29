20d35f4410f8e99d7f33bcec784cbdf0f3a2f305
TomekGlen <45299236+TomekGlen@users.noreply.github.com>
2019-10-29 13:40:52 +0100

    adding task states

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {MDBBtn, MDBIcon } from 'mdbreact';
 
+class NewTask extends Component {
+    minDate = new Date().toISOString().slice(0, 10); //data formatu 'rrrr-mm-dd'
 
-const NewTask = props => {
-    const style = {
-        color: "red"
-    }
+    state = {
+        value: "",
+        desc: "",
+		checked: false,
+		date: this.minDate
+    };
+    
+    handleChange = e => {
+		const id = e.target.id;
 
-	return(
-        <div className="mb-4" style={{border:"1px solid #EEE", padding:"20px", background:"#F8F8F8"}}>
+		if (id === "formTitle") {
+			this.setState({
+                value: e.target.value,
+			});
+		} else if (id === "formDesc") {
+			this.setState({
+                desc: e.target.value,
+            });
+        }  else if (id === "checkboxForm") {
+			this.setState({
+				checked: e.target.checked
+            });
+        }else if (id === "formDate") {
+                this.setState({
+                    date: e.target.value
+                });
+		}
+    };    
+	render() {
+
+        return (
+        <div className="form mb-4" style={{border:"1px solid #EEE", padding:"20px", background:"#F8F8F8"}}>
                     <h4>Dodaj nowe zadanie</h4>
                 <hr/>
                 <div className="mb-2">
@@ -17,6 +44,8 @@ const NewTask = props => {
                         type="text"
                         id="formTitle"
                         className="form-control"
+                        value = {this.state.value}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <div className="mb-2">
@@ -25,7 +54,9 @@ const NewTask = props => {
                         type="text"
                         id="formDesc"
                         className="form-control"
-                    />
+                        onChange={this.handleChange}
+                        defaultValue={this.state.desc}
+                    ></textarea>
                 </div>
                 <div className="mb-4">
                     <label htmlFor="formDate" className="grey-text" style={{fontSize:"80%"}}>Termin zadania</label>
@@ -33,22 +64,33 @@ const NewTask = props => {
                         type="date"
                         id="formDate"
                         className="form-control"
+                        min={this.minDate}
+                        value={this.state.date}
+                        onChange={this.handleChange}
                     />
                 </div>
-                <div>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="checkboxForm"/>
-                        <label class="custom-control-label" for="checkboxForm" style={{fontSize:"90%",paddingTop:"2px"}}>Wysoki priorytet</label>
-                    </div>
+                <div className="custom-control custom-checkbox">
+                    <input 
+                        type="checkbox" 
+                        className="custom-control-input" 
+                        id="checkboxForm"
+                        value={this.state.checked}
+                        onChange={this.handleChange}
+                    />
+                    <label className="custom-control-label" htmlFor="checkboxForm" style={{fontSize:"90%",paddingTop:"2px"}}>Wysoki priorytet</label>
                 </div>
-                <div className="text-left mt-3">
-                    <MDBBtn className="btn btn-teal ml-0" type="submit">
+
+                <div className="text-left mt-4">
+                <p style = {{color: "#999", fontSize : "80%"}}>Klikając poniższy przycisk - zadanie które chcesz dodać będzie widoczne na liście zadań do zrealizowania.</p>
+
+                    <MDBBtn className="btn btn-teal ml-0">
                         Dodaj nowe zadanie
                         <MDBIcon far icon="paper-plane" className="ml-2" />
                     </MDBBtn>
                 </div>
         </div>
-    )
+        )
+    }
 };
 
 export default NewTask;
